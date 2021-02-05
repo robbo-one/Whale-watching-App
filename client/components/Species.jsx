3301f1d5642aca1d07ef0f6ee9c496efb3083f94
student <student@eda-bc-03.localdomain>
2021-02-05 14:22:31 +1300

    add species info

--- client/components/Species.jsx
+++ client/components/Species.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 // import { getSpeciesAndLocations } from '../apis/whale'
 import { getSightingsOfSpecies } from '../apis/whale-hotline'
+import { getSpecies } from '../apis/whale'
+
 
 const Species = (props) => {
     const name = props.match.params.name
     name.replace(" ", "%20")
     
-
-
     const [sightings, getSightings] = useState([])
+    const[dataSet, setDataSet] = useState([])
+    const[speciesPicture, setSpeciesPicture] = useState("")
 
     const updateSpecies = () => {
         getSightingsOfSpecies(name)
@@ -17,19 +19,32 @@ const Species = (props) => {
         })
     }
 
+    const fetchSpecies = () => {
+      getSpecies()
+      .then(data => {
+        setDataSet(data)
+        data.map(species => {
+          if (props.match.params.name == species.name)
+          setSpeciesPicture(species.url)
+        })
+      })
+    }
+
     useEffect(() => {
         updateSpecies()
+        fetchSpecies()
     }, [])
-
 
     return (
         <>
+        <img style={{width: "300px"}}src={speciesPicture}></img>
            <p>Species</p> 
            <ul>
                {sightings.map(sightings => {
                    return <li key={sightings.id}>
                    <strong>Description:</strong> {sightings.description} <br></br> 
                    <strong>Location:</strong> {sightings.location}<br></br><br></br>
+                     {/* <img src={sightings.url}></img> */}
                    </li> 
                })}
            </ul>
